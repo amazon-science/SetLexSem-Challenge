6f9d2cf7182c3b2926cafceb11e77cd8bc85e1df
Manish Gawali <mdgawali@amazon.com>
2024-11-08 19:52:52 +0000

    Adjust the setup file to avoid installing NLTK in the build process and instead install it only when the package is actually being useed

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
-import pathlib
-
 from setuptools import find_packages, setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-
-
-def install_nltk_corpora():
-    """Install NLTK word corpus and wordnet."""
-    import nltk
-
-    home_dir = pathlib.Path.home() / "nltk_data"
-    home_dir.mkdir(parents=True, exist_ok=True)
-    nltk.download("words", download_dir=home_dir)
-    nltk.download("wordnet", download_dir=home_dir)
-
-
-class InstallNltkWordnetDuringInstall(install):
-    def run(self):
-        # Run the standard install process
-        install.run(self)
-        install_nltk_corpora()
-
-
-class InstallNltkWordnetDuringDevelop(develop):
-    def run(self):
-        # Run the standard install process
-        develop.run(self)
-        install_nltk_corpora()
-
 
 setup(
     name="setlexsem",
@@ -37,7 +8,6 @@ setup(
         "tqdm",
         "anthropic",
         "boto3",
-        "nltk",
         "tiktoken",
         "openai",
         "pandas",
@@ -46,9 +16,6 @@ setup(
     extras_require={
         "dev": ["check-manifest", "flake8", "black"],
         "test": ["pytest", "coverage"],
-    },
-    cmdclass={
-        "install": InstallNltkWordnetDuringInstall,
-        "develop": InstallNltkWordnetDuringDevelop,
+        "nltk": ["nltk"],
     },
 )
